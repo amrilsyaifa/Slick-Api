0c813defe35082e28a5b70b7caee91f69534a0f1
amrilsyaifa <amrilsyaifa@gmail.com>
2018-10-10 08:34:05 +0700

    PUT, DELETE /api/products/:id

--- controllers/product.js
+++ controllers/product.js
@@ -23,13 +23,36 @@ const product = app => {
     })
 
     //Read
-    app.get('/api/products', (req,res) => {
+    app.get('/api/products', (req,res) => { 
         res.json(_products)
     })
 
     //Update
+    app.put('/api/products/:id', (req,res) => {
+        //proses update
+        const index = _.findIndex(_products, {
+            id: parseInt(req.params.id)
+        })
+
+        console.log(index)
+
+        //akan merusa data data lama di timpa
+        // _products[index] = req.body
+
+        //solusi tidak merusak data dengan lodash merge
+        _.merge(_products[index], req.body)        
+
+        res.json({"info" : "Success Updated"})
+    })
 
     //Delete
+    app.delete('/api/products/:id', (req, res) => {
+
+        _.remove(_products, tes => {
+            return tes.id === parseInt(req.params.id)
+        })
+        res.json({"info" : "Succed Removed"})
+    })
 }
 
 module.exports = product
